55b99230083b1d5457e5ff85afb8fa169cb83f17
Yanhui Shen <shen.elf@gmail.com>
2012-07-31 17:09:37 +0800

    * almost done

--- eb.cc
+++ eb.cc
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -7,6 +8,7 @@
 #include <string>
 #include <iostream>
 #include <unordered_set>
+#include <unordered_map>
 #include <regex>
 #include <future>
 #include <atomic>
@@ -16,11 +18,6 @@ using namespace std;
 #include "scx/FileInfo.hpp"
 using namespace scx;
 
-const char* const CC = "clang";
-const char* const CXX = "clang++";
-const char* const CXX_FLAG = "-std=c++11 -stdlib=libc++";
-const char* const LD = "clang++";
-const char* const LD_FLAG = "-lc++";
 const unordered_set<string> C_EXT = { "c" };
 const unordered_set<string> CXX_EXT = { "cc", "cxx", "cpp", "C" };
 
@@ -88,29 +85,115 @@ bool CalcDep(ConsUnit& u, const string& c, const string& f)
         return false;
 }
 
+void Usage(const string& cmd)
+{
+    const string sp(cmd.size(), ' ');
+    cout << ""
+        "Usage:\n"
+        "\t" + cmd + " [ file1 file1 | dir1 dir2 ]\n"
+        "\t" + sp + " cc=?        C compiler.\n"
+        "\t" + sp + " cxx=?       C++ compiler.\n"
+        "\t" + sp + " flag=?      Compiler flag.\n"
+        "\t" + sp + " ldflag=?    Linker flag.\n"
+        //"   " + sp + " with=?,?,? without=?,?,?\n"
+        //"   " + sp + " obj=\"outA:dep1, dep2:cmdA; outB:dep1, dep2:cmdB; ...\"\n"
+        "\t" + sp + " shared      Generate shared library.\n"
+        "\t" + sp + " debug       Build with debug symbols.\n"
+        "\t" + sp + " clean       Clean build output.\n"
+        "\t" + sp + " help        Show this help message.\n";
+}
+
 int main(int argc, char** argv)
 {
-    /*
-     * USAGE:
-     * eb (dir) cc=? cxx=? ccflag=? cxxflag=? ld=? 
-     *          with=?;?;? without=?;?;? obj=?;?;? clean force
-     */
+    unordered_map<string, string> ArgTable = {
+        { "out", "app" },
+        { "with", "" },
+        { "without", "" },
+        { "cc", "cc" },
+        { "cxx", "c++" },
+        { "flag", "" },
+        { "ld", "cc" },
+        { "ldflag", "" }
+    };
 
-    const string prjRoot(argc >= 2 ? argv[1] : ".");
+    bool shared = false;
+    bool debug = false;
+    bool clean = false;
+    vector<string> with, without, all;
+
+    // Parse arguments.
+    for (int i = 1; i < argc; ++i) {
+        const string& arg(argv[i]);
+
+        size_t pos = arg.find('=');
+        if (pos != string::npos) {
+            auto iter = ArgTable.find( arg.substr(0, pos) );
+            if (iter != ArgTable.end()) {
+                iter->second = arg.substr(pos+1);
+                continue;
+            }         
+        } else if (arg == "clean") {
+            clean = true;
+            continue;
+        } else if (arg == "debug") {
+            debug = true;
+            continue;
+        } else if (arg == "shared") {
+            shared = true;
+            continue;
+        } else if (arg == "help") {
+            Usage(argv[0]);
+            return 0;
+        }
+
+        switch ( FileInfo(arg).Type() ) {
+        case FileType::Directory:
+        {
+            auto files = Dir::ListTree(arg);
+            all.reserve(all.size() + files.size());
+            all.insert(all.end(), files.begin(), files.end());
+        }
+            break;
+
+        case FileType::Regular:
+        {
+            all.push_back(arg);
+        }
+            break;
+
+        default:
+        {
+            cout << "WARN: bad argument." << endl;
+            cout << "\targ:" << arg << endl;
+        }
+            break;
+        }
+    }
+
+    if (all.empty())
+        all = Dir::ListTree(".");
 
     // Prepare construct units.
+    bool hasCpp = false;
     vector<ConsUnit> units;
-    for (auto file: Dir::ListTree(prjRoot)) {
+    for (auto file: all) {
         ConsUnit unit { file };
         string compiler, flag;
+
+        if (debug)
+            flag += "-g ";
+        if (shared)
+            flag += "-fPIC ";
 
         // Pick up source file.
         const string ext( FileInfo(file).Suffix() );
         if (C_EXT.find(ext) != C_EXT.end()) {
-            compiler = CC;
+            compiler = ArgTable["cc"];
+            flag += ArgTable["flag"];
         } else if (CXX_EXT.find(ext) != CXX_EXT.end()) {
-            compiler = CXX;
-            flag = CXX_FLAG;
+            compiler = ArgTable["cxx"];
+            flag += ArgTable["flag"];
+            hasCpp = true;
         } else
             continue;
 
@@ -125,6 +208,8 @@ int main(int argc, char** argv)
             return 1;
         }
     }
+    if (hasCpp)
+        ArgTable["ld"] = ArgTable["cxx"];
 
 #ifdef DEBUG
     // Debug info.
@@ -139,36 +224,52 @@ int main(int argc, char** argv)
 #endif
 
     // Let's build them all.
-    unsigned int cpus = sysconf(_SC_NPROCESSORS_ONLN);
+    if (!clean) {
+        unsigned int cpus = sysconf(_SC_NPROCESSORS_ONLN);
 
-    string output = "app";
-    bool hasNew = false;
-    string ldCmd = LD + string(" -o ") + output + " " + string(LD_FLAG) + " ";
+        bool hasNew = false;
+        string ldCmd = ArgTable["ld"] + " -o " + ArgTable["out"] + " " + ArgTable["flag"] + " " + ArgTable["ldflag"] + " ";
 
-    cout << "== Compile All ==" << endl;
-    for (auto unit: units) {
-        bool isDepNewer = false;
-        for (auto dep: unit.deps) {
-            if (IsNewer(dep, unit.out)) {
-                isDepNewer = true;
-                break;
+        if (shared)
+            ldCmd += "-shared ";
+
+        cout << "== Compile All ==" << endl;
+        for (auto unit: units) {
+            bool isDepNewer = false;
+            for (const auto& dep: unit.deps) {
+                if (IsNewer(dep, unit.out)) {
+                    isDepNewer = true;
+                    break;
+                }
             }
+            if ( !FileInfo(unit.out).Exists() || isDepNewer) {
+                cout << unit.build << endl;
+                if (::system( unit.build.c_str() ) != 0)
+                    return 2;
+                hasNew = true;
+            }
+            ldCmd += unit.out + " ";
         }
-        if ( !FileInfo(unit.out).Exists() || isDepNewer) {
-            cout << unit.build << endl;
-            DoCmd(unit.build);
-            hasNew = true;
+
+        cout << "== Generate Output ==" << endl;
+        if (hasNew || !FileInfo(ArgTable["out"]).Exists() ) {
+            cout << ldCmd << endl;
+            if (::system( ldCmd.c_str() ) != 0)
+                return 3;
         }
-        ldCmd += unit.out + " ";
-    }
 
-    cout << "== Generate Output ==" << endl;
-    if (hasNew || !FileInfo(output).Exists() ) {
-        cout << ldCmd << endl;
-        DoCmd(ldCmd);
+        cout << "== Done! ==" << endl;
+    } else {
+        const string& rm("rm -f ");
+        for (const auto& unit: units) {
+            const string& cmd(rm + unit.out);
+            cout << cmd << endl;
+            ::system( cmd.c_str() );
+        }
+        const string& cmd(rm + ArgTable["out"]);
+        cout << cmd << endl;
+        ::system( cmd.c_str() );
     }
-
-    cout << "== Done! ==" << endl;
 
     return 0;
 }
