63f4fe3feacdba20c956312fbbae9afa49020bd7
Yanhui Shen <shen.elf@gmail.com>
2015-08-29 18:08:23 +0800

    ~ simplify the logic

--- ConsUnit.cc
+++ ConsUnit.cc
@@ -2,6 +2,7 @@
 #include "Tools.h"
 
 #include <iostream>
+#include <algorithm>
 
 #include "scx/FileInfo.hpp"
 using namespace scx;
@@ -14,32 +15,21 @@ bool ConsUnit::InitC(ConsUnit& u, const string& c, const string& f)
     const string& re("(\\s)+(\\\\)*(\\s)*");
     
     const auto& l = RegexSplit(deps, re);
-    if (l.size() >= 2) {
-        u.out = FileInfo(u.in).BaseName() + ".o";//l[0].substr(0, l[0].size()-1);
-        u.out = u.dir + u.out;
+    if (l.size() < 2) return false;
 
-        bool need = false;
-        if (FileInfo(u.out).Exists()) {
-            for (size_t i = 1; i < l.size(); ++i) {
-                if (IsNewer(l[i], u.out)) {
-                    need = true;
-                    break;
-                }
-            }
-        } else {
-            need = true;
-        }
+    u.out = FileInfo(u.in).BaseName() + ".o";//l[0].substr(0, l[0].size()-1);
+    u.out = u.dir + u.out;
 
-        if (need) {
-            u.cmd = c + f + " -o " + u.out + " -c " + u.in;
+    bool need = FileInfo(u.out).Exists() ?
+        any_of(l.begin(), l.end(), [&](const auto& d) { return IsNewer(d, u.out); }) : true;
+
+    if (need) {
+        u.cmd = c + f + " -o " + u.out + " -c " + u.in;
 #ifdef DEBUG
-            u.deps.assign(l.begin()+1, l.end());
+        u.deps.assign(l.begin()+1, l.end());
 #endif
-        }
-        return true;
-    } else {
-        return false;
     }
+    return true;
 }
 
 bool ConsUnit::InitCpp(ConsUnit& unit, const string& compiler, const string& flag)
