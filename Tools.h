143a1bc5700f6bc7cb6bad6ef4a547950cf5332a
Yanhui Shen <shen.elf@gmail.com>
2017-02-07 11:55:16 +0800

    Add ParseArguments()

--- Tools.h
+++ Tools.h
@@ -8,3 +8,20 @@ auto DoCmd(const std::string& cmd) -> std::string;
 auto RegexSplit(const std::string& str, const std::string& pattern) -> std::vector<std::string>;
 
 auto IsNewer(const std::string& f1, const std::string& f2) -> bool;
+
+template<class OnFlag, class OnKeyValue>
+void ParseArguments(int argc, char** argv, OnFlag onFlag, OnKeyValue onKeyValue)
+{
+    bool cont = true;
+    for (int i = 1; i < argc && cont; ++i) {
+        std::string arg(argv[i]);
+        auto pos = arg.find('=');
+        if (pos != std::string::npos && pos != arg.size() - 1) {
+            std::string key(arg.substr(0, pos));
+            std::string val(arg.substr(pos + 1));
+            cont = onKeyValue(std::move(key), std::move(val));
+        } else {
+            cont = onFlag(std::move(arg));
+        }
+    }
+}
