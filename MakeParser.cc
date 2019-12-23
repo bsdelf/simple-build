742299e462446daaab9a15e8205b4ee4c0134988
Yanhui Shen <shen.elf@gmail.com>
2019-12-23 15:35:58 +0800

    Improve build

---
+++ MakeParser.cc
@@ -0,0 +1,69 @@
+#include "MakeParser.h"
+
+ArgumentParser MakeParser() {
+  ArgumentParser parser;
+  parser
+    .On("clean", "clean files", ArgumentParser::Set("0", "1"))
+    .On("jobs", "set number of jobs", ArgumentParser::Set("0", "0"))
+    .On("target", "set target name", ArgumentParser::Set("a.out", "a.out"))
+    .On("workdir", "set working directory", ArgumentParser::Set(".", "."))
+    .On("verbose", "enable verbose output", ArgumentParser::Set("0", "1"))
+    .Split()
+    .On("as", "set assembler", ArgumentParser::Set("as", "as"))
+    .On("asflags", "add assembler flags", ArgumentParser::Join("", {}))
+    .On("cc", "set c compiler", ArgumentParser::Set("cc", "cc"))
+    .On("cflags", "add c compiler flags", ArgumentParser::Join("", {}))
+    .On("cxx", "set c++ compiler", ArgumentParser::Set("c++", "c++"))
+    .On("cxxflags", "add c++ compiler flags", ArgumentParser::Join("", {}))
+    .On("ld", "set linker", ArgumentParser::Set("cc", "cc"))
+    .On("ldflags", "add linker flags", ArgumentParser::Join("", {}))
+    .On("ldorder", "set linkage order", ArgumentParser::Set("", {}))
+    .On("prefix", "add search directories",
+        ArgumentParser::JoinTo(
+          "cflags", {}, {},
+          [](auto& input) { input = "-I" + input + "/include"; }),
+        ArgumentParser::JoinTo(
+          "cxxflags", {}, {},
+          [](auto& input) { input = "-I" + input + "/include"; }),
+        ArgumentParser::JoinTo(
+          "ldflags", {}, {},
+          [](auto& input) { input = "-L" + input + "/lib"; }))
+    .Split()
+    .On("wol", "without link", ArgumentParser::Set("0", "1"))
+    .On("thread", "use pthreads",
+        ArgumentParser::JoinTo("cflags", {}, "-pthread"),
+        ArgumentParser::JoinTo("cxxflags", {}, "-pthread")
+#ifndef __APPLE__
+          ,
+        ArgumentParser::JoinTo("ldflags", {}, "-pthread")
+#endif
+          )
+    .On("optimize", "set optimize level",
+        ArgumentParser::JoinTo("cflags", {}, "2", [](auto& input) { input = "-O" + input; }),
+        ArgumentParser::JoinTo("cxxflags", {}, "2", [](auto& input) { input = "-O" + input; }))
+    .On("debug", "enable -g",
+        ArgumentParser::JoinTo("cflags", {}, "-g"),
+        ArgumentParser::JoinTo("cxxflags", {}, "-g"))
+    .On("release", "enable -DNDEBUG",
+        ArgumentParser::JoinTo("cflags", {}, "-DNDEBUG"),
+        ArgumentParser::JoinTo("cxxflags", {}, "-DNDEBUG"))
+    .On("strict", "enable -Wall -Wextra -Werror",
+        ArgumentParser::JoinTo("cflags", {}, "-Wall -Wextra -Werror"),
+        ArgumentParser::JoinTo("cxxflags", {}, "-Wall -Wextra -Werror"))
+    .On("shared", "enable -fPIC -shared",
+        ArgumentParser::JoinTo("cflags", {}, "-fPIC"),
+        ArgumentParser::JoinTo("cxxflags", {}, "-fPIC"),
+        ArgumentParser::JoinTo("ldflags", {}, "-shared"))
+    .On("lto", "enable -flto", ArgumentParser::JoinTo("ldflags", {}, "-flto"))
+    .On("c89", "enable -std=c89", ArgumentParser::JoinTo("cflags", {}, "-std=c89"))
+    .On("c99", "enable -std=c99", ArgumentParser::JoinTo("cflags", {}, "-std=c99"))
+    .On("c11", "enable -std=c11", ArgumentParser::JoinTo("cflags", {}, "-std=c11"))
+    .On("c18", "enable -std=c18", ArgumentParser::JoinTo("cflags", {}, "-std=c18"))
+    .On("c++11", "enable -std=c++11", ArgumentParser::JoinTo("cxxflags", {}, "-std=c++11"))
+    .On("c++14", "enable -std=c++14", ArgumentParser::JoinTo("cxxflags", {}, "-std=c++14"))
+    .On("c++17", "enable -std=c++17", ArgumentParser::JoinTo("cxxflags", {}, "-std=c++17"))
+    .On("c++20", "enable -std=c++20", ArgumentParser::JoinTo("cxxflags", {}, "-std=c++20"))
+    .Split()
+    .On("help", "show help", ArgumentParser::Set("0", "1"));
+  return parser;
+}
--- main.cc
+++ main.cc
@@ -10,6 +10,7 @@
 
 #include "ArgumentParser.h"
 #include "Executor.h"
+#include "MakeParser.h"
 #include "SourceAnalyzer.h"
 #include "Utils.h"
 
@@ -19,102 +20,37 @@ using namespace ccbb;
 int main(int argc, char* argv[]) {
   std::vector<std::string> input_paths;
 
-  ArgumentParser parser;
-  parser
-    .On("clean", "clean files", ArgumentParser::Set("0", "1"))
-    .On("jobs", "set number of jobs", ArgumentParser::Set("0", "0"))
-    .On("target", "set target name", ArgumentParser::Set("a.out", "a.out"))
-    .On("workdir", "set working directory", ArgumentParser::Set(".", "."))
-    .On("verbose", "enable verbose output", ArgumentParser::Set("0", "1"))
-    .Split()
-    .On("as", "set assembler", ArgumentParser::Set("as", "as"))
-    .On("asflags", "add assembler flags", ArgumentParser::Join("", {}))
-    .On("cc", "set c compiler", ArgumentParser::Set("cc", "cc"))
-    .On("cflags", "add c compiler flags", ArgumentParser::Join("", {}))
-    .On("cxx", "set c++ compiler", ArgumentParser::Set("c++", "c++"))
-    .On("cxxflags", "add c++ compiler flags", ArgumentParser::Join("", {}))
-    .On("ld", "set linker", ArgumentParser::Set("cc", "cc"))
-    .On("ldflags", "add linker flags", ArgumentParser::Join("", {}))
-    .On("ldorder", "set linkage order", ArgumentParser::Set("", {}))
-    .On("prefix", "add search directories",
-        ArgumentParser::JoinTo(
-          "cflags", {}, {},
-          [](auto& input) { input = "-I" + input + "/include"; }),
-        ArgumentParser::JoinTo(
-          "cxxflags", {}, {},
-          [](auto& input) { input = "-I" + input + "/include"; }),
-        ArgumentParser::JoinTo(
-          "ldflags", {}, {},
-          [](auto& input) { input = "-L" + input + "/lib"; }))
-    .Split()
-    .On("wol", "without link", ArgumentParser::Set("0", "1"))
-    .On("thread", "use pthreads",
-        ArgumentParser::JoinTo("cflags", {}, "-pthread"),
-        ArgumentParser::JoinTo("cxxflags", {}, "-pthread")
-#ifndef __APPLE__
-          ,
-        ArgumentParser::JoinTo("ldflags", {}, "-pthread")
-#endif
-          )
-    .On("optimize", "set optimize level",
-        ArgumentParser::JoinTo("cflags", {}, "2", [](auto& input) { input = "-O" + input; }),
-        ArgumentParser::JoinTo("cxxflags", {}, "2", [](auto& input) { input = "-O" + input; }))
-    .On("debug", "enable -g",
-        ArgumentParser::JoinTo("cflags", {}, "-g"),
-        ArgumentParser::JoinTo("cxxflags", {}, "-g"))
-    .On("release", "enable -DNDEBUG",
-        ArgumentParser::JoinTo("cflags", {}, "-DNDEBUG"),
-        ArgumentParser::JoinTo("cxxflags", {}, "-DNDEBUG"))
-    .On("strict", "enable -Wall -Wextra -Werror",
-        ArgumentParser::JoinTo("cflags", {}, "-Wall -Wextra -Werror"),
-        ArgumentParser::JoinTo("cxxflags", {}, "-Wall -Wextra -Werror"))
-    .On("shared", "enable -fPIC -shared",
-        ArgumentParser::JoinTo("cflags", {}, "-fPIC"),
-        ArgumentParser::JoinTo("cxxflags", {}, "-fPIC"),
-        ArgumentParser::JoinTo("ldflags", {}, "-shared"))
-    .On("lto", "enable -flto", ArgumentParser::JoinTo("ldflags", {}, "-flto"))
-    .On("c89", "enable -std=c89", ArgumentParser::JoinTo("cflags", {}, "-std=c89"))
-    .On("c99", "enable -std=c99", ArgumentParser::JoinTo("cflags", {}, "-std=c99"))
-    .On("c11", "enable -std=c11", ArgumentParser::JoinTo("cflags", {}, "-std=c11"))
-    .On("c18", "enable -std=c18", ArgumentParser::JoinTo("cflags", {}, "-std=c18"))
-    .On("c++11", "enable -std=c++11", ArgumentParser::JoinTo("cxxflags", {}, "-std=c++11"))
-    .On("c++14", "enable -std=c++14", ArgumentParser::JoinTo("cxxflags", {}, "-std=c++14"))
-    .On("c++17", "enable -std=c++17", ArgumentParser::JoinTo("cxxflags", {}, "-std=c++17"))
-    .On("c++20", "enable -std=c++20", ArgumentParser::JoinTo("cxxflags", {}, "-std=c++20"))
-    .Split()
-    .On("help", "show help", ArgumentParser::Set("0", "1"))
-    .OnUnknown([&](std::string key, std::optional<std::string> value) {
-      if (!value &&
-          (std::filesystem::is_regular_file(key) ||
-           std::filesystem::is_directory(key))) {
-        input_paths.push_back(std::move(key));
-      } else {
-        std::cerr << "(E) invalid argument: " << key;
-        if (value) {
-          std::cerr << "=" << *value;
+  auto args =
+    MakeParser()
+      .OnUnknown([&](std::string key, std::optional<std::string> value) {
+        if (!value &&
+            (std::filesystem::is_regular_file(key) ||
+             std::filesystem::is_directory(key))) {
+          input_paths.push_back(std::move(key));
+        } else {
+          std::cerr << "(E) invalid argument: " << key;
+          if (value) {
+            std::cerr << "=" << *value;
+          }
+          std::cerr << std::endl;
+          std::exit(EXIT_FAILURE);
         }
-        std::cerr << std::endl;
-        std::exit(EXIT_FAILURE);
-      }
-    })
-    .Parse(argc, argv);
+      })
+      .Parse(argc, argv)
+      .Data();
 
-  auto args = parser.Data();
   const auto is_verbose = args.at("verbose") == "1";
   const auto without_link = args.at("wol") == "1";
   const auto target = std::filesystem::path(args.at("workdir")) / std::filesystem::path(args.at("target"));
 
-  Executor executor;
-  executor.Start(std::stoul(args.at("jobs")));
-
   // show help
   if (args.at("help") == "1") {
-    const int n = 8;
+    ArgumentParser::FormatHelpOptions options{4, 4, "\n"};
     std::cout
-      << "Usage:" << std::endl
-      << std::string(n, ' ') << std::string(argv[0]) << " [option ...] [file ...] [directory ...]" << std::endl
-      << std::endl
-      << parser.FormatHelp(ArgumentParser::FormatHelpOptions{.space_before_key = n})
+      << "Usage:\n\n"
+      << std::string(options.space_before_key, ' ') << std::string(argv[0]) << " [options...] [files...] [directories...]\n\n"
+      << "Options:\n\n"
+      << MakeParser().FormatHelp(options)
       << std::endl;
     std::exit(EXIT_SUCCESS);
   }
@@ -147,10 +83,10 @@ int main(int argc, char* argv[]) {
   }
 
   // gather source files
+  std::vector<std::string> source_paths;
   if (input_paths.empty()) {
     input_paths.push_back(".");
   }
-  std::vector<std::string> source_paths;
   for (const auto& path : input_paths) {
     if (std::filesystem::is_regular_file(path)) {
       source_paths.push_back(path);
@@ -177,6 +113,9 @@ int main(int argc, char* argv[]) {
     std::cout << "(W) no souce files" << std::endl;
     std::exit(EXIT_SUCCESS);
   }
+
+  Executor executor;
+  executor.Start(std::stoul(args.at("jobs")));
 
   // analyze source files
   std::vector<SourceFile> new_files;
